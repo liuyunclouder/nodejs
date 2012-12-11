6d6e6487bfa61d6b7b9401b72698ffe31c52e84c
lingwu.tyf@taobao.com <lingwu.tyf@taobao.com>
2012-12-11 12:00:05 +0800

    add grunt

--- proj/cloudblog/routes/index.js
+++ proj/cloudblog/routes/index.js
@@ -132,6 +132,19 @@ module.exports = function (app) {
     });
 
 
+    app.get('/upload_socket', function (req, res) {
+        fs.readFile(__dirname + '/../index.html',
+        function (err, data) {
+          if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+          }
+          res.writeHead(200);
+          res.end(data);
+        });
+    });
+
+
 
     function checkLogin (req, res, next) {
         if (req.cookies['uname'] && Users.indexOf(req.cookies['uname']) !== -1) {
---
+++ proj/test/dist/test.js
@@ -0,0 +1,24 @@
+/*! Test - v0.1.0 - 2012-12-11
+* https://github.com/lingwu.tyf/test
+* Copyright (c) 2012 liuyunclouder; Licensed MIT */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
